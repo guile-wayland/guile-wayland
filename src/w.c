d7866ff936dd5b7149c47793f50e536bd90f9542
Z572 <873216071@qq.com>
2022-02-09 22:33:30 +0800

    u

--- src/w.c
+++ src/w.c
@@ -1,14 +1,23 @@
 #include <libguile.h>
 #include <wayland-server-core.h>
 
-SCM
-a_wrapper (SCM x)
+
+
+void
+wl_event_loop_destroy_wrapper (SCM loop)
 {
-  return scm_fr;
+  return wl_event_loop_destroy((struct wl_event_loop*)scm_to_pointer(loop));
+}
+
+SCM
+wl_event_loop_create_wrapper (void)
+{
+  return scm_from_pointer(wl_event_loop_create() , NULL);
 }
 
 void
-init_wayland (void)
+init_w (void)
 {
-  scm_c_define_gsubr ("wl-event-loop-create" ,0,0,0 , a_wrapper)
+  scm_c_define_gsubr ("wl-event-loop-create" ,0,0,0 , wl_event_loop_create_wrapper);
+  scm_c_define_gsubr ("wl-event-loop-destroy" ,1,0,0 , wl_event_loop_destroy_wrapper);
 }
