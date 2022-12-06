2c6f07efa6e369661afa2de46c4a5324e92edf7d
Z572 <873216071@qq.com>
2022-12-06 12:15:58 +0800

    lisp/wayland/listener.c: %make-wl-listener: use scm_gc_protect_object.

--- lisp/wayland/listener.c
+++ lisp/wayland/listener.c
@@ -10,12 +10,16 @@
 
 SCM_DEFINE(scm_make_wl_listener,"%make-wl-listener",1,0,0,(SCM proc),""){
   struct wl_listener *new_listener=scm_gc_malloc(sizeof(struct wl_listener), "wl-listener");
-  new_listener->notify=scm_to_pointer(scm_procedure_to_pointer(scm_c_public_ref("system foreign", "void"),
-                                                proc,
-                                                scm_list_2(scm_from_utf8_symbol("*"),
-                                                           scm_from_utf8_symbol("*"))));
-  return scm_call_1(scm_c_public_ref("wayland listener", "wrap-wl-listener"),
-                    scm_from_pointer(new_listener,NULL));
+  SCM p=scm_procedure_to_pointer(scm_c_public_ref("system foreign", "void"),
+                                 proc,
+                                 scm_list_2(scm_from_utf8_symbol("*"),
+                                            scm_from_utf8_symbol("*")));
+  SCM pp=scm_from_pointer(new_listener,NULL);
+  new_listener->notify=scm_to_pointer(p);
+  SCM sl=scm_call_1(scm_c_public_ref("wayland listener", "wrap-wl-listener"),
+                    pp);
+  scm_gc_protect_object(sl);
+  return sl;
 }
 
 void scm_init_wl_listener(void){
