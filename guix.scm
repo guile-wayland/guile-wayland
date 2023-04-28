3afbdca8bb4fdc6bfb34a6881d8049aad6b4b2b8
Z572 <zhengjunjie@iscas.ac.cn>
2023-04-28 10:33:41 +0800

    update guix.scm

--- guix.scm
+++ guix.scm
@@ -16,6 +16,36 @@
  (gnu packages file)
  (gnu packages build-tools))
 
+(define-public guile-bytestructure-class
+  (package
+    (name "guile-bytestructure-class")
+    (version "0.1")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/Z572/guile-bytestructure-class")
+                    (commit (string-append "v" version))))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "0lmpckqisz3viakbzwa659l86l64iyf98qskbi8ggf0m1p46rmmw"))))
+    (build-system gnu-build-system)
+    (arguments
+     (list #:make-flags #~'("GUILE_AUTO_COMPILE=0")))
+    (native-inputs
+     (list autoconf
+           automake
+           pkg-config
+           guile-3.0-latest))
+    (inputs (list guile-3.0-latest))
+    (propagated-inputs (list guile-bytestructures))
+    (synopsis "bytestructure and goops")
+    (description "This package combines bytestructure with goops,
+and provide 4 new bytestructure-descriptor:
+bs:unknow, cstring-pointer*, bs:enum, stdbool.")
+    (home-page "https://github.com/Z572/guile-bytestructure-class")
+    (license license:gpl3+)))
+
 (define guile-wayland
   (package
     (name "guile-wayland")
@@ -50,9 +80,11 @@
     (inputs (list guile-3.0-latest wayland))
     (propagated-inputs
      (list
-      (primitive-load
-       (string-append (dirname (dirname (current-filename)))
-                      "/guile-bytestructure-class/guix.scm"))
+      (let ((file (string-append (dirname (dirname (current-filename)))
+                                 "/guile-bytestructure-class/guix.scm")))
+        (if (file-exists? file)
+            (primitive-load file)
+            guile-bytestructure-class))
       guile-bytestructures))
     (synopsis "")
     (description "")
