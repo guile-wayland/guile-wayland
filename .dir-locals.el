c5e87d1d356da4e57b888f8477a3fba5d86ca5b5
Grigory Shepelev <shegeley@gmail.com>
2023-07-16 08:12:07 +0300

    .dir-locals.el: basic emacs identation standarts

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;;; .dir-locals.el -- Per-directory local variables for GNU Emacs 23 and later.
+
+((nil         . ((fill-column . 80)
+                 (tab-width . 4)))
+ (c-mode      . ((c-file-style . "gnu")))
+ (scheme-mode . ((indent-tabs-mode . nil))))
+
+;;; .dir-locals.el ends here
