f35306a9aab18c98f6647866c10210d0b95d1005
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2023-07-30 20:31:23 +0800

    move (wayland client) code to (wayland server client).

--- lisp/wayland/client.scm
+++ lisp/wayland/client.scm
@@ -1,112 +1,13 @@
-(define-module (wayland client)
-  #:use-module (bytestructures guile)
-  #:use-module (wayland display)
-  #:use-module (wayland listener)
-  #:use-module (wayland resource)
-  #:use-module (wayland base)
-  #:use-module (wayland config)
-  #:use-module (wayland list)
-  #:use-module (wayland util)
-  #:use-module (ice-9 format)
-  #:use-module (srfi srfi-1)
-  #:use-module (srfi srfi-9)
-  #:use-module (srfi srfi-26)
-  #:use-module ((system foreign)
-                #:select
-                (null-pointer?
-                 bytevector->pointer
-                 make-pointer
-                 procedure->pointer
-                 define-wrapped-pointer-type
-                 pointer->procedure
-                 pointer->bytevector
-                 pointer->string
-                 string->pointer
-                 sizeof
-                 %null-pointer
-                 dereference-pointer
-                 pointer-address
-                 void
-                 (int . ffi:int)
-                 (double . ffi:double)
-                 (uint32 . ffi:uint32)
-                 (size_t . ffi:size_t)
-                 (uintptr_t . ffi:uintptr_t)))
-  #:export (%wl-client-struct
-            wl-client-create
-            wrap-wl-client
-            unwrap-wl-client
-            wl-client-add-destroy-listener
-            wl-client-from-link
-            wl-client-get-link
-            wl-client-get-display))
+(define-module (wayland client))
 
-(define %wl-client-struct (bs:unknow))
+(eval-when (eval load compile)
+  (begin
+    (let* ((current-module (current-module))
+           (current-module-interface (resolve-interface (module-name current-module)))
+           (submodule-interface (resolve-interface '(wayland server client))))
+      (module-use! current-module submodule-interface)
+      (module-use! current-module-interface submodule-interface))))
 
-(define-bytestructure-class <wl-client> ()
-  %wl-client-struct
-  wrap-wl-client unwrap-wl-client wl-client?)
 
-(define %wl-client-create
-  (wayland-server->procedure '* "wl_client_create" (list '* ffi:int)))
-
-(define (wl-client-create w-display fd)
-  (wrap-wl-client (%wl-client-create (unwrap-wl-display w-display) fd)))
-
-(define %wl-client-flush
-  (wayland-server->procedure void "wl_client_flush" '(*)))
-
-(define (wl-client-flush client)
-  (%wl-client-flush (unwrap-wl-client client)))
-
-(define %wl-client-get-link
-  (wayland-server->procedure '* "wl_client_get_link" '(*)))
-
-(define (wl-client-get-link client)
-  (wrap-wl-list (%wl-client-get-link (unwrap-wl-client client))))
-
-(define-wl-server-procedure (wl-client-add-destroy-listener client listener)
-  (void "wl_client_add_destroy_listener" '(* *))
-  (% (unwrap-wl-client client) (unwrap-wl-listener listener)))
-
-(define %wl-client-from-link
-  (wayland-server->procedure '* "wl_client_from_link" '(*)))
-
-(define (wl-client-from-link wl-l)
-  (wrap-wl-client (%wl-client-from-link (unwrap-wl-list wl-l))))
 
 ;; "wl_client_get_credentials"
-
-(define wl-event-queue-destroy
-  (wayland-client->procedure void "wl_event_queue_destroy" '(*)))
-
-(define wl-client-get-object
-  (let ((proc (wayland-server->procedure '* "wl_client_get_object" (list '* ffi:uint32))))
-    (lambda (a b)
-      (wrap-wl-resource (proc (unwrap-wl-client a) b )))))
-
-(define wl-client-post-no-memory
-  (let ((proc (wayland-server->procedure '* "wl_client_post_no_memory" (list '*))))
-    (lambda (client)
-      (wrap-wl-resource (proc (unwrap-wl-client client))))))
-
-(define wl-client-post-implementation-error
-  (let ((proc (wayland-server->procedure '* "wl_client_post_implementation_error" (list '* '*))))
-    (lambda (client msg)
-      (wrap-wl-resource (proc (unwrap-wl-client client) (string->pointer msg))))))
-
-(define wl-client-add-resource-created-listener
-  (let ((proc (wayland-server->procedure void "wl_client_add_resource_created_listener" '(* *))))
-    (lambda (client listener)
-      (proc (unwrap-wl-client client) (unwrap-wl-listener listener)))))
-
-(define wl-client-for-each-resource
-  (let ((proc (wayland-server->procedure void "wl_client_for_each_resource" (list '* '*))))
-    (lambda (client iterator)
-      (proc (unwrap-wl-client client) iterator))))
-
-(define %wl-client-get-display
-  (wayland-server->procedure '* "wl_client_get_display" '(*)))
-
-(define (wl-client-get-display client)
-  (wrap-wl-display (%wl-client-get-display (unwrap-wl-client client))))
---
+++ lisp/wayland/server/client.scm
@@ -0,0 +1,77 @@
+(define-module (wayland server client)
+  #:use-module (bytestructures guile)
+  #:use-module (wayland display)
+  #:use-module (wayland listener)
+  #:use-module (wayland resource)
+  #:use-module (wayland base)
+  #:use-module (wayland list)
+  #:use-module (wayland util)
+  #:use-module ((system foreign) #:prefix ffi:)
+  #:export (%wl-client-struct
+            wl-client-create
+            wrap-wl-client
+            unwrap-wl-client))
+
+(define %wl-client-struct (bs:unknow))
+
+(define-bytestructure-class <wl-client> ()
+  %wl-client-struct
+  wrap-wl-client unwrap-wl-client wl-client?)
+
+(define-wl-server-procedure (wl-client-create w-display fd)
+  ('* "wl_client_create" (list '* ffi:int))
+  (wrap-wl-client (% (unwrap-wl-display w-display) fd)))
+
+(define-wl-server-procedure (wl-client-get-link client)
+  ('* "wl_client_get_link" '(*))
+  (wrap-wl-list (% (unwrap-wl-client client))))
+
+(define-wl-server-procedure (wl-client-from-link wlist)
+  ('* "wl_client_from_link" '(*))
+  (wrap-wl-client (% (unwrap-wl-list wlist))))
+
+(define-wl-server-procedure (wl-client-destroy client)
+  (ffi:void "wl_client_destroy" (list '*))
+  (% (unwrap-wl-client client)))
+
+(define-wl-server-procedure (wl-client-flush client)
+  (ffi:void "wl_client_flush" '(*))
+  (% (unwrap-wl-client client)))
+
+;; wl_client_get_credentials
+
+;; wl_client_get_fd
+
+(define-wl-server-procedure (wl-client-add-destroy-listener client listener)
+  (ffi:void "wl_client_add_destroy_listener" '(* *))
+  (% (unwrap-wl-client client) (unwrap-wl-listener listener)))
+
+;; wl_client_get_destroy_listener
+
+(define-wl-server-procedure (wl-client-get-object client id)
+  ('* "wl_client_get_object" (list '* ffi:uint32))
+  (wrap-wl-resource (% (unwrap-wl-client client) id)))
+
+(define-wl-server-procedure (wl-client-post-no-memory client)
+  ('* "wl_client_post_no_memory" (list '*))
+  (wrap-wl-resource (% (unwrap-wl-client client))))
+
+(define-wl-server-procedure (wl-client-post-implementation-error client msg)
+  ('* "wl_client_post_implementation_error" (list '* '*))
+  (wrap-wl-resource (% (unwrap-wl-client client) (ffi:string->pointer msg))))
+
+(define wl-client-add-resource-created-listener
+  (let ((proc (wayland-server->procedure ffi:void "wl_client_add_resource_created_listener"
+                                         '(* *))))
+    (lambda (client listener)
+      (proc (unwrap-wl-client client) (unwrap-wl-listener listener)))))
+
+(define wl-client-for-each-resource
+  (let ((proc (wayland-server->procedure ffi:void "wl_client_for_each_resource"
+                                         (list '* '*))))
+    (lambda (client iterator)
+      (proc (unwrap-wl-client client) iterator))))
+
+(define-wl-server-procedure (wl-client-get-display client)
+  ('* "wl_client_get_display" '(*))
+  (wrap-wl-display (% (unwrap-wl-client client))))
