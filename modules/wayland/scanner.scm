6badfb58c752746d8f4619d509b2c7ecbf5f587c
Grigory Shepelev <shegeley@gmail.com>
2023-10-16 17:24:31 +0300

    wayland/scanner: <event name="reset"> handling

--- modules/wayland/scanner.scm
+++ modules/wayland/scanner.scm
@@ -107,6 +107,12 @@
                                      (and arg-interface-type
                                           (_->- arg-interface-type))
                                      (->bool allow-null)) ...)))
+    ((event (@ (name ,name) (since (,since #f)) (type (,type #f))))
+     (%make-message (_->- name)
+                    (and type (string=? type "destructor"))
+                    since
+                    'event
+                    (list '())))
     ((request (@ (name ,name) (since (,since #f)) (type (,type #f)))
               (arg (@ (type ,arg-type)
                       (name ,(_->- -> arg-name))
