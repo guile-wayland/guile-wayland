4f31c018c58ea2f0161c98c0b77686f20f32a8c8
Grigory Shepelev <shegeley@gmail.com>
2023-10-29 01:51:51 +0300

    guix.scm: expose wlroots protocols (xmls)

--- guix.scm
+++ guix.scm
@@ -16,6 +16,7 @@
  (gnu packages linux)
  (gnu packages xdisorg)
  (gnu packages gl)
+ (gnu packages package-management)
  (gnu packages admin)
  (gnu packages xorg)
  (gnu packages pkg-config)
@@ -24,72 +25,77 @@
  (gnu packages build-tools))
 
 (define-public wlroots
-  (package
-    (name "wlroots")
-    (version "0.16.2")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://gitlab.freedesktop.org/wlroots/wlroots")
-             (commit version)))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32 "1m12nv6avgnz626h3giqp6gcx44w1wq6z0jy780mx8z255ic7q15"))))
-    (build-system meson-build-system)
-    (arguments
-     `(#:phases
-       (modify-phases %standard-phases
-         (add-before 'configure 'hardcode-paths
-           (lambda* (#:key inputs #:allow-other-keys)
-             (substitute* "xwayland/server.c"
-               (("Xwayland") (string-append (assoc-ref inputs
-                                                       "xorg-server-xwayland")
-                                            "/bin/Xwayland")))
-             #t))
-         (add-before 'configure 'fix-meson-file
-           (lambda* (#:key native-inputs inputs #:allow-other-keys)
-             (substitute* "backend/drm/meson.build"
-               (("/usr/share/hwdata/pnp.ids")
-                (string-append (assoc-ref (or native-inputs inputs) "hwdata")
-                               "/share/hwdata/pnp.ids")))))
-         (add-before 'configure 'copy-protocols
-           ;; NOTE: exposed protocols files
-           (lambda* (#:key inputs outputs #:allow-other-keys)
-             (mkdir-p "protocols")
-             (copy-recursively
-              "protocol" ;; why singular?!
-              (string-append (assoc-ref outputs "out")
-                             "/protocols"))
-             #t))
-         )))
-    (propagated-inputs
-     (list ;; As required by wlroots.pc.
-      eudev
-      libinput-minimal
-      libxkbcommon
-      mesa
-      pixman
-      libseat
-      wayland
-      wayland-protocols
-      xcb-util-errors
-      xcb-util-wm
-      xorg-server-xwayland))
-    (native-inputs
-     (cons*
-      `(,hwdata "pnp")
-      pkg-config
-      wayland
-      (if (%current-target-system)
-          (list pkg-config-for-build)
-          '())))
-    (home-page "https://gitlab.freedesktop.org/wlroots/wlroots/")
-    (synopsis "Pluggable, composable, unopinionated modules for building a
+  ;; NOTE: had to rewrite to export XML protocols to reference them later
+  (let [(expose-protocols
+         `(lambda* (#:key inputs outputs #:allow-other-keys)
+           (mkdir-p "protocols")
+           (copy-recursively
+            "protocol" ;; why singular?!
+            (string-append (assoc-ref outputs "out")
+                           "/protocols")
+            #:copy-file (lambda (x y)
+                          (if (string-suffix-ci? ".xml" x)
+                              (copy-file x y)
+                              (lambda (x y) #f)))) #t))]
+    (package
+      (name "wlroots")
+      (version "0.16.2")
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://gitlab.freedesktop.org/wlroots/wlroots")
+               (commit version)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "1m12nv6avgnz626h3giqp6gcx44w1wq6z0jy780mx8z255ic7q15"))))
+      (build-system meson-build-system)
+      (arguments
+       `(#:phases
+         (modify-phases %standard-phases
+           (add-before 'configure 'hardcode-paths
+             (lambda* (#:key inputs #:allow-other-keys)
+               (substitute* "xwayland/server.c"
+                 (("Xwayland")
+                  (string-append
+                   (assoc-ref inputs "xorg-server-xwayland")
+                   "/bin/Xwayland")))
+               #t))
+           (add-before 'configure 'fix-meson-file
+             (lambda* (#:key native-inputs inputs #:allow-other-keys)
+               (substitute* "backend/drm/meson.build"
+                 (("/usr/share/hwdata/pnp.ids")
+                  (string-append (assoc-ref (or native-inputs inputs) "hwdata")
+                                 "/share/hwdata/pnp.ids")))))
+           (add-before 'configure 'expose-protocols
+             ,expose-protocols))))
+      (propagated-inputs
+       (list ;; As required by wlroots.pc.
+        eudev
+        libinput-minimal
+        libxkbcommon
+        mesa
+        pixman
+        libseat
+        wayland
+        wayland-protocols
+        xcb-util-errors
+        xcb-util-wm
+        xorg-server-xwayland))
+      (native-inputs
+       (cons*
+        `(,hwdata "pnp")
+        pkg-config
+        wayland
+        (if (%current-target-system)
+            (list pkg-config-for-build)
+            '())))
+      (home-page "https://gitlab.freedesktop.org/wlroots/wlroots/")
+      (synopsis "Pluggable, composable, unopinionated modules for building a
 Wayland compositor")
-    (description "wlroots is a set of pluggable, composable, unopinionated
+      (description "wlroots is a set of pluggable, composable, unopinionated
 modules for building a Wayland compositor.")
-    (license license:expat)))
+      (license license:expat))))
 
 (define %source-dir (dirname (current-filename)))
 
@@ -123,56 +129,81 @@ bs:unknow, cstring-pointer*, bs:enum, stdbool.")
     (home-page "https://github.com/Z572/guile-bytestructure-class")
     (license license:gpl3+)))
 
+(define xmls-sources
+  `(("wayland-protocols" . "/share/wayland-protocols")
+    ("wlroots" . "/protocols")))
+
+
 (define guile-wayland
-  (package
-    (name "guile-wayland")
-    (version "0.0.2")
-    (source (local-file %source-dir "guile-wayland-checkout"
-                        #:recursive? #t
-                        #:select? (git-predicate %source-dir)))
-    (build-system gnu-build-system)
-    (arguments
-     (list
-      #:configure-flags '(list "--disable-static")
-      #:make-flags '(list "GUILE_AUTO_COMPILE=0")
-      #:phases
-      #~(modify-phases %standard-phases
-          (add-before 'configure 'expose-protocols
-            (lambda* (#:key inputs #:allow-other-keys)
-              (substitute* "modules/wayland/config.scm.in"
-                (("@WAYLAND_PROTOCOLS_DATAROOTDIR@")
-                 (string-append
-                  (assoc-ref %build-inputs "wlroots")
-                  "/protocols")))
-              #t))
-          (add-before 'build 'load-extension
-            (lambda* (#:key outputs #:allow-other-keys)
-              (let* ((out (assoc-ref outputs "out"))
-                     (lib (string-append out "/lib")))
-                (invoke "make" "install"
-                        "-C" "libguile-wayland"
-                        "-j" (number->string
-                              (parallel-job-count)))
-                (substitute* (find-files "." "\\.scm$")
-                  (("\"libguile-wayland\"")
-                   (string-append "\"" lib "/libguile-wayland\"")))))))))
-    (native-inputs
-     (list
-      autoconf
-      automake
-      libtool
-      pkg-config
-      texinfo
-      guile-3.0-latest))
-    (inputs (list guile-3.0-latest wayland wayland-protocols
-                  wlroots))
-    (propagated-inputs
-     (list
-      guile-bytestructure-class
-      guile-bytestructures))
-    (synopsis "")
-    (description "")
-    (home-page "https://github.com/guile-wayland/guile-wayland")
-    (license license:gpl3+)))
+  (let [(expose-protocols
+         #~(lambda* (#:key inputs outputs #:allow-other-keys)
+             (let* [(out (assoc-ref outputs "out"))
+                    (xml-dir (string-append out "/xmls"))]
+               (mkdir-p xml-dir)
+               (map
+                (lambda (x)
+                  (let [(xmls
+                         (find-files
+                          (string-append
+                           (assoc-ref inputs (car x))
+                           (cdr x))
+                          (lambda (x _) (string-suffix-ci? ".xml" x))))]
+                    (map (lambda (x)
+                           (copy-file
+                            x
+                            (string-append xml-dir "/" (basename x)))) xmls)))
+                (quote #$xmls-sources))
+               (substitute* "modules/wayland/config.scm.in"
+                            (("@WAYLAND_PROTOCOLS_DATAROOTDIR@") xml-dir))
+               #t)))]
+    (package
+     (name "guile-wayland")
+     (version "0.0.2")
+     (source (local-file %source-dir "guile-wayland-checkout"
+                         #:recursive? #t
+                         #:select? (git-predicate %source-dir)))
+     (build-system gnu-build-system)
+     (arguments
+      (list
+       #:configure-flags '(list "--disable-static")
+       #:make-flags '(list "GUILE_AUTO_COMPILE=0")
+       #:phases
+       #~(modify-phases %standard-phases
+                        (add-before 'configure 'expose-protocols
+                                    #$expose-protocols)
+                        (add-before 'build 'load-extension
+                                    (lambda* (#:key outputs #:allow-other-keys)
+                                      (let* ((out (assoc-ref outputs "out"))
+                                             (lib (string-append out "/lib")))
+                                        (invoke "make" "install"
+                                                "-C" "libguile-wayland"
+                                                "-j" (number->string
+                                                      (parallel-job-count)))
+                                        (substitute* (find-files "." "\\.scm$")
+                                                     (("\"libguile-wayland\"")
+                                                      (string-append "\"" lib "/libguile-wayland\"")))))))))
+     (native-inputs
+      (list
+       autoconf
+       automake
+       libtool
+       pkg-config
+       texinfo
+       guile-3.0-latest))
+     (inputs
+      (list
+       guix
+       guile-3.0-latest
+       wayland
+       wayland-protocols
+       wlroots))
+     (propagated-inputs
+      (list
+       guile-bytestructure-class
+       guile-bytestructures))
+     (synopsis "")
+     (description "")
+     (home-page "https://github.com/guile-wayland/guile-wayland")
+     (license license:gpl3+))))
 
 guile-wayland
