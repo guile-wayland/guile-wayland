7816ad1412b6591d8ce33231d60fc1de69b1de1b
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2023-11-05 17:37:44 +0800

    support use GUILE_WAYLAND_PROTOCOL_PATH env to find protocols.

--- modules/wayland/client/protocol/xdg-shell.scm
+++ modules/wayland/client/protocol/xdg-shell.scm
@@ -3,6 +3,6 @@
   #:use-module (bytestructure-class)
   #:use-module (wayland scanner)
   #:use-module (wayland config))
-(eval-when (compile)
-  (define xdg-shell.xml (string-append %wayland-protocols-dir "/wayland-protocols/stable/xdg-shell/xdg-shell.xml")))
-(use-wayland-protocol (xdg-shell.xml #:type client))
+(use-wayland-protocol
+ ("wayland-protocols/stable/xdg-shell/xdg-shell.xml"
+  #:type client))
